aeef7878e32993fa1abff4981d0fb6bc853b8111
iwih <osama.a.shammari@gmail.com>
2017-09-20 00:20:54 +0300

    Syncing

--- app/src/main/java/com/iwih/materialdesignexcersices/MainActivity.java
+++ app/src/main/java/com/iwih/materialdesignexcersices/MainActivity.java
@@ -1,7 +1,24 @@
 package com.iwih.materialdesignexcersices;
 
+import android.animation.Animator;
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
